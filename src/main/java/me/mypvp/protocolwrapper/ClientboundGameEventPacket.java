a7b9dee8fdf98c72f503b6b360b3e428d376f4ea
APICode06 <blau.fefran270805@gmail.com>
2023-01-29 11:08:13 +0100

    Add game event packet

---
+++ src/main/java/me/mypvp/protocolwrapper/ClientboundGameEventPacket.java
@@ -0,0 +1,59 @@
+package me.mypvp.protocolwrapper;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.PacketType.Play.Server;
+import com.comphenix.protocol.events.InternalStructure;
+import com.comphenix.protocol.events.PacketContainer;
+import me.mypvp.protocolwrapper.types.GameEventAction;
+import org.jetbrains.annotations.NotNull;
+
+public class ClientboundGameEventPacket extends AbstractPacket {
+
+  public static final PacketType TYPE = Server.GAME_STATE_CHANGE;
+
+  private final PacketField<InternalStructure> eventField = new PacketField<>(getContainer().getStructures(), 0);
+  private final PacketField<Float> valueField = new PacketField<>(getContainer().getFloat(), 0);
+
+  public ClientboundGameEventPacket() {
+  }
+
+  public ClientboundGameEventPacket(@NotNull Object handle) {
+    super(handle);
+  }
+
+  public ClientboundGameEventPacket(
+      @NotNull PacketContainer packetContainer) {
+    super(packetContainer);
+  }
+
+  @Override
+  public PacketType getType() {
+    return TYPE;
+  }
+
+  public void setValue(float value) {
+    valueField.write(value);
+  }
+
+  public float readValue() {
+    return valueField.read();
+  }
+
+  public void setGameAction(GameEventAction gameAction) {
+    InternalStructure structure = eventField.read();
+    structure.getIntegers().write(0, gameAction.getId());
+    eventField.write(structure);
+  }
+
+  public GameEventAction getGameAction() {
+    InternalStructure structure = eventField.read();
+    int key = structure.getIntegers().read(0);
+    for (GameEventAction value : GameEventAction.values()) {
+      if(value.getId() == key) {
+        return value;
+      }
+    }
+    return null;
+  }
+
+}
